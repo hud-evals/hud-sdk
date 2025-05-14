f15f0eca50d8d7de44a73aa17ebc84a1ecc26697
mxunknown <mxunknown@gmail.com>
2025-05-14 17:14:29 +0300

    custom envs: reshuffle structure, Image works via MCP inspector

---
+++ environments/novnc_ubuntu/controller/server.py
@@ -0,0 +1,23 @@
+from io import BytesIO
+import pyautogui
+from controller.pyautogui_rosetta import PyAutoGUIRosetta
+
+from typing import Any
+from mcp.server.fastmcp import FastMCP, Image
+
+mcp = FastMCP("StatefulServer", port=8483)
+
+@mcp.tool("step")
+def step(action: list[dict[str, Any]]) -> Image:
+    """
+    Execute a sequence of actions.
+    """
+    pyautogui_rosetta = PyAutoGUIRosetta()
+    pyautogui_rosetta.execute_sequence(action)
+
+    buffer = BytesIO()
+    pyautogui.screenshot().save(buffer, format="PNG")
+    return Image(data=buffer.getvalue(), format="png")
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
