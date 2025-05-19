4f84a463562183e6058a786be4d6a4988215cdd0
mxunknown <mxunknown@gmail.com>
2025-05-19 17:53:14 +0300

    examples: update local; server: simplify args

--- environments/novnc_ubuntu/controller/server.py
+++ environments/novnc_ubuntu/controller/server.py
@@ -3,15 +3,15 @@ import pyautogui
 from controller.pyautogui_rosetta import PyAutoGUIRosetta
 
 from typing import Any
-from mcp.server.fastmcp import FastMCP, Image
+from mcp.server.fastmcp import Context, FastMCP, Image
 
 server = FastMCP("StatefulServer", port=8483)
 
 
 @server.tool("step")
-def step(actions: list[dict[str, Any]]) -> Image:
+def step(action: dict[str, Any]) -> Image:
     pyautogui_rosetta = PyAutoGUIRosetta()
-    pyautogui_rosetta.execute_sequence(actions)
+    pyautogui_rosetta.execute_sequence([action])
 
     buffer = BytesIO()
     pyautogui.screenshot().save(buffer, format="PNG")
