fe958fbf5c92fd21a31d04f55a397eea115ac907
socpite <socpite@gmail.com>
2025-06-02 22:09:53 +0700

    formatting

--- hud/tests/env/test_local_docker_client.py
+++ hud/tests/env/test_local_docker_client.py
@@ -90,6 +90,7 @@ async def test_container_starting_times_out():
     with pytest.raises(TimeoutError, match="not healthy"):
         await wait_until_healthy_container(container, timeout=1)  # small timeout for speed
 
+
 class MockContainer:
     def __init__(self, id: str):
         self.id = id
@@ -101,18 +102,18 @@ class MockContainer:
             "Config": {"Healthcheck": {"Interval": 1_000_000}},
             "State": {"Health": {"Status": "healthy"}, "Status": "running"},
         }
-    
+
     def log(self, **kwargs):
         return fake_log_generator(self.log_lines)
 
     async def start(self):
         await asyncio.sleep(0.5)  # Simulate async start
-    
+
     def check_logs(self, caplog):
         for line in self.log_lines:
             assert f"container {self.id} | {line}" in caplog.text
-        
-    
+
+
 @pytest.mark.asyncio
 async def test_create_local_docker_client(mocker, caplog):
     caplog.set_level("INFO")
@@ -129,16 +130,14 @@ async def test_create_local_docker_client(mocker, caplog):
 
     mock_create = mocker.patch(
         "hud.env.local_docker_client.aiodocker.containers.DockerContainers.create",
-        return_value=container
+        return_value=container,
     )
     mock_wait_until_healthy_container = mocker.patch(
-    "hud.env.local_docker_client.wait_until_healthy_container",
+        "hud.env.local_docker_client.wait_until_healthy_container",
     )
-    spy_stream_logs = mocker.spy(
-        hud.env.local_docker_client, "_stream_logs"
-    )
+    spy_stream_logs = mocker.spy(hud.env.local_docker_client, "_stream_logs")
 
-    client = await LocalDockerClient.create("test-image:latest",host_config=host_config)
+    client = await LocalDockerClient.create("test-image:latest", host_config=host_config)
     assert isinstance(client, LocalDockerClient)
     assert client.container_id == "123abc456def"
 
@@ -146,11 +145,3 @@ async def test_create_local_docker_client(mocker, caplog):
     mock_wait_until_healthy_container.assert_called_once_with(container, 1)
     spy_stream_logs.assert_called_once_with(container)
     container.check_logs(caplog)
-
-
-
-
-
-
-    
-
