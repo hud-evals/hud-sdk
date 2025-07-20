3815d885ddddfb71072fed07139f02658c211d31
Jaideep <jdchawla29@gmail.com>
2025-07-20 04:05:00 +0000

    moved stuff

--- examples/hud_env/train.py
+++ examples/hud_env/train.py
@@ -6,10 +6,10 @@ from hud import Task
 
 """
 inference:
-CUDA_VISIBLE_DEVICES=0 vf-vllm --model Qwen/Qwen3-0.6B
+NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 NCCL_SHM_DISABLE=1 CUDA_VISIBLE_DEVICES=0 uv run vf-vllm --model Qwen/Qwen3-0.6B
 
 training:
-CUDA_VISIBLE_DEVICES=1 accelerate launch --num-processes 1 --config-file configs/zero3.yaml train.py
+NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 NCCL_SHM_DISABLE=1 CUDA_VISIBLE_DEVICES=1 uv run accelerate launch --num-processes 1 --config-file verifiers/configs/zero3.yaml hud_env/train.py
 """
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -25,14 +25,16 @@ with open(eval_tasks_file, 'r') as f:
 tasks = [Task.from_dict(task_data) for task_data in train_tasks_data]
 eval_tasks = [Task.from_dict(task_data) for task_data in eval_tasks_data]
 
+EVAL_TASKS = 100
+
 # Create HUDGym instance
 vf_env = HUDGym(
     tasks=tasks,
-    eval_tasks=eval_tasks,
+    eval_tasks=eval_tasks[:EVAL_TASKS],
     adapter=BasicAdapter(),
 )
 
-model_name = "Qwen/Qwen3-0.6B"
+model_name = "willcb/Qwen3-0.6B"
 run_name = "gsm8k-grpo_" + model_name.split("/")[-1].lower()
 
 model, tokenizer = vf.get_model_and_tokenizer(model_name)
@@ -50,6 +52,7 @@ training_args.save_steps = 100
 training_args.max_steps = 200
 training_args.eval_strategy = "steps"
 training_args.eval_steps = 10
+training_args.max_concurrent = 96
 
 trainer = vf.GRPOTrainer(
     model=model,
